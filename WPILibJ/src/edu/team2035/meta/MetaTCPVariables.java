a1cfa0737a4b5870e36c9c674bbbbb54bd21dd0a
toastertaster <toastertaster@gmail.com>
2012-02-03 01:20:30 -0800

    Tonights debugging

--- WPILibJ/src/edu/team2035/meta/MetaTCPVariables.java
+++ WPILibJ/src/edu/team2035/meta/MetaTCPVariables.java
@@ -8,6 +8,7 @@
 package edu.team2035.meta;
 
 import com.sun.squawk.io.BufferedReader;
+import com.sun.squawk.util.SquawkHashtable;
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.Hashtable;
@@ -20,6 +21,7 @@ import javax.microedition.io.ServerSocketConnection;
 import edu.wpi.first.wpilibj.DriverStationLCD;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import javax.microedition.io.*;
 
 /** MetaTCPVariables
  * For receiving variables from the dashboard.
@@ -29,16 +31,24 @@ import java.io.InputStreamReader;
 public class MetaTCPVariables {
 
     Vector connections;
-    Hashtable variables;
+    SquawkHashtable variables;
     int numberOfConnections;
     InputStream dashboardStream;
     InputStreamReader reader;
     BufferedReader buffRead;
     Thread connect;
     ServerSocketConnection server;
+    UDPDatagramConnection server2;
+    
     SocketConnection socket;
+    
+    InputStreamReader isr;
+    private int lastByte;
+    private boolean updaterunning;
 
-    static final int PORT = 1130;  // TCP listen connection port
+    public static final int PORT = 1130;  // TCP listen connection port
+    
+    public float x1, x2, x3, x4, y1, y2, y3, y4, range;
     
     /**
      * Starting the constructor causes it to accept new connections and
@@ -47,10 +57,14 @@ public class MetaTCPVariables {
     public MetaTCPVariables()
     {
        server = null;
+       server2 = null;
        connections = new Vector(); // Vector of open connections.
-       variables = new Hashtable(); // Hastable of key/value pairs to store values
+       variables = new SquawkHashtable(); // Hastable of key/value pairs to store values
        numberOfConnections = 0;
        buffRead = null;
+       isr = null;
+       updaterunning = false;
+       x1 = x2 = x3 = x4 = y1 = y2 = y3 = y4 = range = (float)0.0;
        
        connect = new Thread() {
            public void run() {
@@ -59,16 +73,17 @@ public class MetaTCPVariables {
        };
        connect.start();
        
-       new Thread() {
-           public void run() {
-               update();
+//       new Thread() {
+//           public void run() {
+//               update();
 //               try {
-//                    //Thread.sleep(200);
+//                    Thread.sleep(2);
+//                    System.out.println("update sleep");
 //                } catch (InterruptedException ex1) {
-//                    // TBD: what to do here?
+//                    System.out.println("update except");
 //                }
-           }
-       }.start(); 
+//           }
+//       }.start(); 
     }
     
     /** newVariableValue
@@ -76,7 +91,7 @@ public class MetaTCPVariables {
      * @param s called the key, this is String such as "positionx"
      * @param o called the value, this is an Object that stores its content
      */
-    private synchronized void newVariableValue(String s, Object o)
+    private void newVariableValue(String s, Object o)
     {
         variables.put(s, o);
     }
@@ -86,7 +101,7 @@ public class MetaTCPVariables {
      * @param s called the key, this is the String identifer of the object of interest such as "rectanglelength" 
      * @return The Object.  The Object will need to be cast in its use.
      */
-    public synchronized Object getVariableValue(String s)
+    public Object getVariableValue(String s)
     {
         return variables.get(s);
     }
@@ -96,28 +111,34 @@ public class MetaTCPVariables {
      * @param s called the key, this is the String identifer of the object of interest such as "rectanglelength" 
      * @return a float value of the object or -99 if there is an error.
      */
-    public synchronized float getVariableFloatValue(String s)
+    public float getVariableFloatValue(String s)
     {
-        Object o = variables.get(s);
-        if (o instanceof Float)
-        {
-            return ((Float)o).floatValue();
-        }
+//        Object o = variables.get(s);
+//        if (o instanceof Float)
+//        {
+//            return ((Float)o).floatValue();
+//        }
+        Float o = (Float)variables.get(s);
+        if (o != null)
+            return o.floatValue();
         return (float)-99.0;
     }
 
     /**
      * Accepts UDP connections to the robot on the specified port.
      */
-    private synchronized void acceptConnections() {
-
+    private synchronized void acceptConnections() 
+    {
+        System.out.println("acceptConnections");
         // Open the server
         while (true) {
             try {
                 server = (ServerSocketConnection) Connector.open("socket://:" + PORT);
+                //server2 = (UDPDatagramConnection) Connector.open("datagram://:1131");
+                System.out.println("Connector open");
                 break;
             } catch (IOException ex) {
-                ex.printStackTrace();
+                //ex.printStackTrace();
                 try {
                     
                     Thread.sleep(2000);
@@ -128,33 +149,77 @@ public class MetaTCPVariables {
         }
 
         try {
-            while (true) {
+            //while (true) {
                 // Wait for a connection
                 socket = (SocketConnection) server.acceptAndOpen();
 
-                socket.setSocketOption(SocketConnection.LINGER, 0);
-            }
+                //socket.setSocketOption(SocketConnection.LINGER, 0);
+                //socket.setSocketOption(SocketConnection.KEEPALIVE, 100);
+                //socket.setSocketOption(SocketConnection.RCVBUF, 4096);
+                System.out.println("socket option");
+                
+            //}
         } catch (IOException ex) {
             System.out.println("MetaTCP: LOST SERVER!");
+            ex.printStackTrace();
         }
         try {
             
             dashboardStream = socket.openInputStream();
+            System.out.println("Input stream");
             DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser3, 1, "Listening on " + server.getLocalAddress());
         } catch (IOException ex){
-            
+            System.out.println("Input stream fail");
         }
         DriverStationLCD.getInstance().updateLCD();
         connections.addElement(server);
         numberOfConnections++;
-            
-        buffRead = new BufferedReader(new InputStreamReader(dashboardStream));
-                   
+        
+        isr = new InputStreamReader(dashboardStream);
+        buffRead = new BufferedReader(isr);
+        
+        
+        System.out.println("buffRead created");
+        if (updaterunning == false)
+        {
+            updaterunning = true;
+            new Thread() {
+            public void run() {
+                while(true) {
+                    update();
+    //               try {
+    //                    Thread.sleep(2);
+    //                    System.out.println("update sleep");
+    //                } catch (InterruptedException ex1) {
+    //                    System.out.println("update except");
+    //                }
+                    //System.out.println("update 2");
+                    }
+                }
+            }.start(); 
+        }
+        
     }
     
     public int getConnections(){
-        
+        //System.out.println("number of connections = " + numberOfConnections);        
         return numberOfConnections;
+    }
+    
+    public int getCount() 
+    {
+        return variables.size();
+    }
+    
+    public float getrange()
+    {
+        return this.range;
+        
+    }
+    
+    public float getx2()
+    {
+        return this.x2;
     }
     
     /**
@@ -162,22 +227,32 @@ public class MetaTCPVariables {
      * It is assumed that only one key is updated per UDP datagram.
      * This only handles float variables at the present time.
      */
-    public synchronized void update()
+    public void update()
     {
+        //System.out.println("update");
         //Enumeration e = connections.elements();
         //while( e.hasMoreElements() )
         //{
             //Object Enum = e.nextElement();
             //if (Enum instanceof SocketConnection)
             //{
-            if(buffRead != null)
+        if(buffRead != null)
+        {
+            try 
             {
-                try {
+                while (buffRead.ready())
+                {
+                //System.out.println("buffRead != null");
                     
-                    String message = buffRead.readLine();
-                    StringTokenizer st = new StringTokenizer(message, " \n\r\t\f");
-                    if (st.tokenCount == 9)
+                    String message;
+                    //String message = readString();
+                    //System.out.println("readLine");
+                    while ((message = buffRead.readLine()) != null) 
                     {
+                        StringTokenizer st = new StringTokenizer(message, " \n\r\t\f");
+                        System.out.println("ST " + st.countTokens() + " " + message);
+                        if (st.tokenCount == 9)
+                        {
 //                        String cmd = st.nextToken();
 //                        if ((cmd.toLowerCase().compareTo("float") == 0))
 //                        {
@@ -191,40 +266,52 @@ public class MetaTCPVariables {
                          * This handles that using the switch statement for each
                          * different value to store them in a hashtable
                          */
-                        for (int i = 0; i < 9; i++)
-                        {
-                            String value = st.nextToken();
-                            Float temp = Float.valueOf(value);
-                            float f = Float.valueOf(value).floatValue();
-                            switch (i)
+                            for (int i = 0; i < 9; i++)
                             {
-                                case 0: newVariableValue("range", temp); break;
-                                case 1: newVariableValue("x1", temp); break;
-                                case 2: newVariableValue("y1", temp); break;
-                                case 3: newVariableValue("x2", temp); break;
-                                case 4: newVariableValue("y2", temp); break;
-                                case 5: newVariableValue("x3", temp); break;
-                                case 6: newVariableValue("y3", temp); break;
-                                case 7: newVariableValue("x4", temp); break;
-                                case 8: newVariableValue("y4", temp); break;
+                                String value = st.nextToken();
+                                Float temp = Float.valueOf(value);
+                                float f = Float.valueOf(value).floatValue();
+                                //System.out.println("for loop " + i + " " + f);
+                                switch (i)
+                                {
+                                    case 0: 
+                                        newVariableValue("range", temp);
+                                        //synchronized (this) {
+                                        this.range = f; 
+                                        //}
+                                        System.out.println("for loop " + i + " " + f + " " + this.range); 
+                                        break;
+                                    case 1: 
+                                        newVariableValue("x1", temp); 
+                                        this.x1 = f; 
+                                        break;
+                                    case 2: 
+                                        newVariableValue("y1", temp); 
+                                        this.y1 = f; 
+                                        break;
+                                    case 3: newVariableValue("x2", temp); this.x2 = f; break;
+                                    case 4: newVariableValue("y2", temp); this.y2 = f; break;
+                                    case 5: newVariableValue("x3", temp); this.x3 = f; break;
+                                    case 6: newVariableValue("y3", temp); this.y3 = f; break;
+                                    case 7: newVariableValue("x4", temp); this.x4 = f; break;
+                                    case 8: newVariableValue("y4", temp); this.y4 = f; break;
+                                }
                             }
                         }
-                                    
-                            
-
                         
                         // TODO: If handling other data types than floats,
                         // those would be done here.
                     }
-                    
-                } catch (IOException z) {
-                    // TBD: what to do?
                 }
+                    
+            } catch (IOException z) {
+                // TBD: what to do?
+                System.out.println("update fail");
+            }
             }
             //}
         //}
         
     }
-    
 
 }
